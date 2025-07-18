32ede81573f6935b682d036a7bf27a8d07de0d5d
Ahmed <ahmedrehan505ii@gmail.com>
2025-07-18 14:07:39 +0500

    Initial commit: add calculator project

---
+++ main.py
@@ -0,0 +1,17 @@
+number1 = int(input("Enter the value of no. = "))
+number2 = int(input("Enter the value of second no. = "))
+
+add = number1 + number2
+print("the sum of", number1, "and", number2, "is", add)
+
+sub = number1 - number2
+print("the subtraction of", number1, "and", number2, "is", sub)
+
+mul = number1 * number2
+print("the multiplication of", number1, "and", number2, "is", mul)
+
+div = number1 / number2
+print("the division of", number1, "and", number2, "is", div)
+
+flr = number1 // number2
+print("the floor division of", number1, "and", number2, "is", flr)
